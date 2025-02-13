da5fbc6abf4ee75707a8a37bfc14d5b6ffc77a13
Vivek Kadam <vk2567298@gmail.com>
2025-02-13 12:29:01 +0530

    domain remove

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -34,7 +34,7 @@ router.get("/logout", (req, res) => {
             httpOnly: true,
             secure: true,
             sameSite: "None",
-            domain: "paste-client.vercel.app", // Set this to your domain (optional)
+            // domain: "localhost:5173", // Set this to your domain (optional)
             path: "/", // Ensure it's removed for all paths
         });
         console.log(req.cookies.loginCookie);
