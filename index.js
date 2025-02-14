0fe63456acae4cf7e7570702e1ab69c1c7c0d9b8
Vivek Kadam <vk2567298@gmail.com>
2025-02-14 11:39:37 +0530

    netlify link correction

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const port=process.env.PORT || 4000;
 
 app.use(cookieParser());
 
-const allowedOrigins = ["http://localhost:5173", "https://paste-client.vercel.app","https://pasteclient.onrender.com","https://elaborate-marigold-94bf88.netlify.app","https://dancing-donut-f03689.netlify.app","https://createpaste.netlify.app/"];
+const allowedOrigins = ["http://localhost:5173", "https://paste-client.vercel.app","https://pasteclient.onrender.com","https://elaborate-marigold-94bf88.netlify.app","https://dancing-donut-f03689.netlify.app","https://createpaste.netlify.app"];
 app.use(cors({
     origin: allowedOrigins, // Allow requests from your frontend
     credentials: true, // Allow cookies and authentication headers
